8e47cc78371f0f641d6cef106e5100d46fedcd37
Tae Eun Jeong <147980144+nundung@users.noreply.github.com>
2024-04-14 21:27:25 +0900

    Refactor: 클래스 함수 분리

--- src/service/post.service.js
+++ src/service/post.service.js
@@ -1,5 +1,6 @@
 const { pool } = require('../config/postgres');
 const { NotFoundException, ForbiddenException } = require('../modules/Exception');
+const { Post } = require('../entity/postEntity');
 
 /**
  * @typedef {{
@@ -20,85 +21,6 @@ const { NotFoundException, ForbiddenException } = require('../modules/Exception'
 /**
  * @typedef {Omit<Post, 'content'>} SelectPost
  */
-
-class Post {
-    /**
-     * 게시글 인덱스
-     * @type {number}
-     */
-    idx;
-
-    /**
-     * 게시글 제목
-     * @type {string}
-     */
-    title;
-
-    /**
-     * 게시글 내용
-     * @type {string}
-     */
-    content;
-
-    /**
-     * 게시글 생성날짜
-     * @type {Date}
-     */
-    createdAt;
-
-    /**
-     * 게시글 조회수
-     * @type {number}
-     */
-    view;
-
-    /**
-     * 게시글 작성자
-     * @type {{
-     *  idx: number
-     * }}
-     */
-    author;
-
-    /**
-     * 게시글이 속한 게시판 정보
-     * @type {{
-     *  idx: number
-     * }}
-     */
-    game;
-
-    constructor(data) {
-        this.idx = data.idx;
-        this.title = data.title;
-        this.content = data.content;
-        this.createdAt = data.createdAt;
-        this.view = data.view;
-        this.author = data.author;
-        this.game = data.game;
-    }
-
-    /**
-     * @param {Post} result
-     */
-    static createPost(result) {
-        return new Post({
-            idx: result.idx,
-            title: result.title,
-            content: result.content,
-            createdAt: result.createdAt,
-            view: result.view,
-            author: {
-                idx: result.userIdx,
-                nickname: result.nickname,
-            },
-        });
-    }
-
-    static createPostList(rows) {
-        return rows.map((row) => this.createPost(row));
-    }
-}
 
 /**
  * 게시글 업로드
@@ -204,28 +126,12 @@ const getPostAllByGameIdx = async (gameIdx, getDto) => {
 };
 
 /**
- * 조회수 반영하기
- * @param {number} postIdx
- * @returns {Promise<void>}
- */
-const increasePostViewByIdx = async (postIdx, conn = pool) => {
-    await conn.query(
-        `INSERT INTO view
-            (post_idx, user_idx)
-        VALUES
-            ($1, $2)`,
-        [postIdx, userIdx]
-    );
-    return;
-};
-
-/**
  * 게시글 상세보기
  * @param {number} postIdx 가져올 게시글 인덱스
  * @returns {Promise<Post>}
  */
 const getPostByIdx = async (postIdx, conn = pool) => {
-    const result = await conn.query(
+    const result = await pool.query(
         `SELECT
             post.title,
             post.content,
@@ -258,6 +164,23 @@ const getPostByIdx = async (postIdx, conn = pool) => {
 };
 
 /**
+ * 조회수 반영하기
+ * @param {number} postIdx
+ * @param {number} userIdx
+ * @returns {Promise<void>}
+ */
+const increasePostViewByIdx = async (postIdx, userIdx, conn = pool) => {
+    await pool.query(
+        `INSERT INTO view
+            (post_idx, user_idx)
+        VALUES
+            ($1, $2)`,
+        [postIdx, userIdx]
+    );
+    return;
+};
+
+/**
  * 게시글 검색하기
  */
 
@@ -279,7 +202,6 @@ const deletePostByIdx = async (postIdx, userIdx) => {
 };
 
 module.exports = {
-    Post,
     createPost,
     getPostByIdx,
     deletePostByIdx,
