e9d5ba7b17299a7b5e0195a00689cbb036bc5184
Tae Eun Jeong <147980144+nundung@users.noreply.github.com>
2024-04-23 21:24:48 +0900

    Fix: getPost.dto 수정

--- src/post/dto/getPost.dto.js
+++ src/post/dto/getPost.dto.js
@@ -1,33 +1,25 @@
 module.exports = class GetPostDto {
-    postsPerPage;
     page;
 
     /**
      * @param {
-     *  postsPerPage: number;
      *  page: number;
      * } data
      */
     constructor(data) {
-        this.postsPerPage = data.postsPerPage;
         this.page = data.page;
     }
 
     /**
      * @param {
-     *  postsPerPage: number;
      *  page: number;
      * } data
      */
-    static GetPostDto(data) {
-        if (!data.postsPerPage) {
-            throw new BadRequestException('Invalid postPerPage');
-        }
+    static getPostDto(data) {
         if (!data.page) {
             throw new BadRequestException('Invalid page');
         }
-        return new CreateTodoDto({
-            postPerPage: data.postPerPage,
+        return new GetPostDto({
             page: data.page,
         });
     }
